5770d8aae50acffb73c295ef57937826c83d4894
Tibor Ölvedi <teebeey@gmail.com>
2019-09-05 20:28:36 +0200

    add logout feature

--- enzoldhazam.sh
+++ enzoldhazam.sh
@@ -11,6 +11,9 @@ case "$1" in
     echo ${PASS} > ~/.enzoldhazam/pass
     echo
     ;;
+  logout)
+    rm -rf ~/.enzoldhazam
+    ;;
   settemp)
     USER=`cat ~/.enzoldhazam/user`
     PASS=`cat ~/.enzoldhazam/pass`
@@ -22,7 +25,7 @@ case "$1" in
     curl -X POST -F "h=${THERM}" -F 'save=1' -F "robj[${THERM}]=${TEMP}" -F 'timer=f=&day=&fdata%5Bday-1-1-time%5D=0000&fdata%5Bday-1-1-temp%5D=25&fdata%5Bday-1-0-time%5D=0015&fdata%5Bday-1-0-temp%5D=21&fdata%5Bday-2-1-time%5D=0000&fdata%5Bday-2-1-temp%5D=25&fdata%5Bday-2-0-time%5D=0015&fdata%5Bday-2-0-temp%5D=21&fdata%5Bday-3-1-time%5D=0000&fdata%5Bday-3-1-temp%5D=25&fdata%5Bday-3-0-time%5D=0015&fdata%5Bday-3-0-temp%5D=21&fdata%5Bday-4-1-time%5D=0000&fdata%5Bday-4-1-temp%5D=25&fdata%5Bday-4-0-time%5D=0015&fdata%5Bday-4-0-temp%5D=21&fdata%5Bday-5-1-time%5D=0000&fdata%5Bday-5-1-temp%5D=25&fdata%5Bday-5-0-time%5D=0015&fdata%5Bday-5-0-temp%5D=21&fdata%5Bday-6-1-time%5D=0000&fdata%5Bday-6-1-temp%5D=25&fdata%5Bday-6-0-time%5D=0015&fdata%5Bday-6-0-temp%5D=21&fdata%5Bday-7-1-time%5D=0000&fdata%5Bday-7-1-temp%5D=25&fdata%5Bday-7-0-time%5D=0015&fdata%5Bday-7-0-temp%5D=21' --cookie "${COOKIE}" https://www.enzoldhazam.hu/ajax/thermostats >/dev/null 2>&1
     ;;
   *)
-    echo "Usage:  $0 {login|settemp}"
+    echo "Usage:  $0 {login|settemp|logout}"
     echo "This CLI tool has been written by Tibor Ölvedi <teebeey@gmail.com>"
     exit 1
     ;;
